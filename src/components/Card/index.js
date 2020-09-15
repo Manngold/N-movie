bece369a090ca3c5be97fbe2601ada76889b768f
Manngold <jsw4820@gmail.com>
2020-09-15 22:27:00 +0900

    add image card

---
+++ src/components/Card/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Card({
+  backdropPath,
+  id,
+  title,
+  popularity,
+  posterPath,
+  releaseDate,
+  overview,
+}) {
+  return (
+    <div
+      style={{
+        backgroundImage: `url('https://image.tmdb.org/t/p/w500${posterPath}')`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        height: '500px',
+        width: '500px',
+      }}
+    >
+      <span>{title}</span>
+      <span>{popularity}</span>
+      <span>{releaseDate}</span>
+      <p>{overview}</p>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Card from '../Card';
 
 function Movies({ movies }) {
-  return <div>here is Movies</div>;
+  return (
+    <div>
+      {movies.map((movie) => {
+        const {
+          backdrop_path: backdropPath,
+          id,
+          title,
+          popularity,
+          poster_path: posterPath,
+          release_date: releaseDate,
+          overview,
+        } = movie;
+        return (
+          <Card
+            key={id}
+            backdropPath={backdropPath}
+            id={id}
+            title={title}
+            popularity={popularity}
+            posterPath={posterPath}
+            releaseDate={releaseDate}
+            overview={overview}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
